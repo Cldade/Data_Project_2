7a10dd265b8cccff05bf2e44d589e926973b3f2e
Alejandro <alejandromartilledo1995@gmail.com>
2023-02-02 17:11:48 +0100

    Update camara.py

--- Code/Generator/camara.py
+++ Code/Generator/camara.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-def generateCentralitaData(matricula, timestamp):
+def generateCamaraData(matricula, timestamp):
     matricula = matricula    
     parpadeo = random.randrange(12,21) #nº de parpadeos por minuto. De media son 15/min en estado relajado
     cabeceo = random.randrange(1,5) #nº de cabeceos por minuto. En estado relajado esto no pasa.
